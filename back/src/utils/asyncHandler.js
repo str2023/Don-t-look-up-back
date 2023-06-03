7b994e955d7ef6eef86c28154ada00ce80eb8fbb
[AI7/2반] 손태린 <elice.str@gmail.com>
2023-06-03 11:37:56 +0900

    :recycle: Refactor: dotenv 패키지 스크립트 추가
    Feat: asyncHandler 추가

---
+++ back/src/utils/asyncHandler.js
@@ -0,0 +1,16 @@
+/*
+ * request handler를 async function으로 작성하면서
+ * try ~ catch, next를 자동으로 할 수 있도록 구성
+ * ref) Node_js와_mongoDB_II_Lecture_02_수정
+ */
+const asyncHandler = (requestHandler) => {
+    return async (req, res, next) => {
+      try {
+        await requestHandler(req, res);
+      } catch (err) {
+        next(err);
+      }
+    };
+  };
+  
+  export default asyncHandler;

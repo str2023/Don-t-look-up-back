f08cbbd16f9d1c8ea42bc6aec2d24e4b0f1d3e93
[AI7/2반] 손태린 <elice.str@gmail.com>
2023-06-05 00:54:54 +0900

    :art: Style: 콘솔로그에 색깔입힘

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -9,10 +9,12 @@ mongoose.connect(DB_URL);
 const db = mongoose.connection;
 
 db.on('connected', () =>
-  console.log(`정상적으로 MongoDB 서버에 연결되었습니다. ${DB_URL}`),
+  console.log('\x1b[34mMongoDB 서버에 연결되었습니다.\x1b[0m', DB_URL),
 );
 db.on('error', (error) =>
-  console.error(`MongoDB 연결에 실패하였습니다...\n${DB_URL}\n${error}`),
+  console.error(
+    `\x1b[31mMongoDB 연결에 실패하였습니다.\x1b[0m..\n${DB_URL}\n${error}`,
+  ),
 );
 
 module.exports = User;

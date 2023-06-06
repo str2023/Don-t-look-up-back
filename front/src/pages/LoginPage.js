bd477c73e5e24d950583f02df906b13d2ecb1582
[AI7/2반] 손태린 <elice.str@gmail.com>
2023-06-06 19:52:59 +0900

    :fire: Chore: remove /front

--- front/src/pages/LoginPage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import LoginForm from '../components/user/LoginForm';
-
-const LoginPage = () => {
-  return <LoginForm />;
-};
-
-export default LoginPage;
--- front/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as IntroPage } from './IntroPage';
-export { default as LoginPage } from './LoginPage';
-export { default as RegisterPage } from './RegisterPage';
-export { default as MainPage } from './MainPage';

cd7a44cb8e6c8357aa1dd81f6bc6c1dbfef99320
mjerry <winterfeel97@gmail.com>
2023-06-07 22:34:42 +0900

    :recycle: Refactor: weatherService 변수 부분 리팩토링
    weatherService 변수 부분을 getArea middleware로 리팩토링 완료

--- back/src/routers/weatherRouter.js
+++ back/src/routers/weatherRouter.js
@@ -2,40 +2,45 @@ const weatherRouter = require('express').Router();
 const weatherService = require('../services/weatherService');
 // const loginRequired = require('../middlewares/loginRequired');
 const asyncHandler = require('../utils/asyncHandler');
+const getArea = require('../middlewares/getArea');
 
 // 자외선 지수 라우터. body에 위도, 경도 값이 들어온다.
 weatherRouter.get(
   '/UVIdx',
+  getArea,
   asyncHandler(async (req, res, next) => {
-    const { area } = req.body;
-    const UVIdx = await weatherService.getUVIdx({ area });
+    const { Area } = req.AreaInfo;
+    const UVIdx = await weatherService.getUVIdx({ Area });
     res.status(200).send(UVIdx);
   }),
 );
 
 weatherRouter.get(
   '/UltraSrtNcst',
+  getArea,
   asyncHandler(async (req, res, next) => {
-    const { area } = req.body;
-    const UltraSrtNcst = await weatherService.getUltraSrtNcst({ area });
+    const { Area } = req.AreaInfo;
+    const UltraSrtNcst = await weatherService.getUltraSrtNcst({ Area });
     res.status(200).send(UltraSrtNcst);
   }),
 );
 
 weatherRouter.get(
   '/UltraSrtFcst',
+  getArea,
   asyncHandler(async (req, res, next) => {
-    const { area } = req.body;
-    const UltraSrtFcst = await weatherService.getUltraSrtFcst({ area });
+    const { Area } = req.AreaInfo;
+    const UltraSrtFcst = await weatherService.getUltraSrtFcst({ Area });
     res.status(200).send(UltraSrtFcst);
   }),
 );
 
 weatherRouter.get(
   '/VilageFcst',
+  getArea,
   asyncHandler(async (req, res, next) => {
-    const { area } = req.body;
-    const VilageFcst = await weatherService.getVilageFcst({ area });
+    const { Area } = req.AreaInfo;
+    const VilageFcst = await weatherService.getVilageFcst({ Area });
     res.status(200).send(VilageFcst);
   }),
 );
--- back/src/services/weatherService.js
+++ back/src/services/weatherService.js
@@ -1,16 +1,11 @@
 const api = require('../utils/api');
 const { Weather } = require('../db');
 const { getTime } = require('../utils/getTime');
-const locationService = require('./locationService');
 
 // 자외선 지수
-const getUVIdx = async ({ area }) => {
-  // const areaNo = area || 1100000000; // 추후 카카오맵API 연동
-  // const addressName = '서울'; // 추후 카카오맵API 연동
-
-  const addressInfo = await locationService.getAddressInfo({ area });
-  const areaNo = addressInfo.locationNo;
-  const addressName = addressInfo.locationName;
+const getUVIdx = async ({ Area }) => {
+  const areaNo = Area.No;
+  const addressName = Area.Name;
 
   const time = getTime().YMDH;
   // DB에 있는지 날씨 조회
@@ -40,14 +35,10 @@ const getUVIdx = async ({ area }) => {
 };
 
 // 초단기 실황
-const getUltraSrtNcst = async ({ area }) => {
-  // const { nx, ny } = area || { nx: 55, ny: 127 };
-  // const addressName = '서울'; // 추후 카카오맵API 연동
-
-  const addressInfo = await locationService.getAddressInfo({ area });
-  const nx = addressInfo.x;
-  const ny = addressInfo.y;
-  const addressName = addressInfo.locationName;
+const getUltraSrtNcst = async ({ Area }) => {
+  const nx = Area.x;
+  const ny = Area.y;
+  const addressName = Area.Name;
 
   const time = getTime();
   const date = time.YMD;
@@ -83,9 +74,11 @@ const getUltraSrtNcst = async ({ area }) => {
 };
 
 // 초단기 예보
-const getUltraSrtFcst = async ({ area }) => {
-  const { nx, ny } = area || { nx: 55, ny: 127 };
-  const addressName = '서울';
+const getUltraSrtFcst = async ({ Area }) => {
+  const nx = Area.x;
+  const ny = Area.y;
+  const addressName = Area.Name;
+
   const time = getTime();
   const date = time.YMD;
   const hour = time.HH;
@@ -107,9 +100,11 @@ const getUltraSrtFcst = async ({ area }) => {
 };
 
 // 단기 예보
-const getVilageFcst = async ({ area }) => {
-  const { nx, ny } = area || { nx: 55, ny: 127 };
-  const addressName = '서울';
+const getVilageFcst = async ({ Area }) => {
+  const nx = Area.x;
+  const ny = Area.y;
+  const addressName = Area.Name;
+
   const time = getTime();
   const date = time.YMD;
   const hour = time.HM;

30275babd95a4d22029d4823b77bd5f320b6a482
[AI7/2반] 손태린 <elice.str@gmail.com>
2023-06-12 18:08:24 +0900

    :bug: Fix: 에러 처리

--- back/src/middlewares/getArea.js
+++ back/src/middlewares/getArea.js
@@ -2,7 +2,10 @@ const locationService = require('../services/locationService');
 
 const getArea = async (req, res, next) => {
   const { area } = req.query;
-
+  if (!area) {
+    res.status(400).send('area 요청정보가 없습니다');
+    return;
+  }
   const areaInfo = await locationService.getAddressInfo({ area });
 
   const Area = {
